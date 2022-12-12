aa9e611cb8af34ebba11c9f3529e4597af3ed974
JulianMaiaru <julimaiaru_93@hotmail.com>
2022-12-12 15:59:27 +0100

    12/12/22

--- src/components/Content/Carrito.jsx
+++ src/components/Content/Carrito.jsx
@@ -1,14 +1,25 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { CarritoContext } from '../context/CarritoContext';
-import { consultarBDD } from '../utils/funcionesUtiles';
-import {Link} from 'react-router-dom'
+import { mostrarCarrito } from '../utils/funcionesUtiles';
+import Swal from 'sweetalert2';
+import { Link } from 'react-router-dom'
+import DatosComprador from './DatosComprador';
 
 const Carrito = ({ children }) => {
 
-
-    const { carrito, agregarProducto, quitarProducto } = useContext(CarritoContext)
-
+    const { carrito, agregarProducto, quitarProducto, cleanCart } = useContext(CarritoContext)
     const [carritoLocal, setCarritoLocal] = useState([]);
+
+    const mostrarAlertaEliminar = () => {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Se Elimino el producto al carrito!',
+            showConfirmButton: false,
+            timer: 1700
+        })
+    }
+    
 
     useEffect(() => {
         const prodMostrar = carrito.map(producto =>
@@ -18,19 +29,21 @@ const Carrito = ({ children }) => {
                     <h5 className="card-title">{producto.nombre}</h5>
                     <p className="card-text">Detalles: {producto.detalles}</p>
                     <p className="card-text">Precio: {producto.precio}</p>
+                    <p className="card-text">Cantidad: {producto.cantidad}</p>
                     <p className="card-text">Precio Total: {producto.precio * producto.cantidad}</p>
                     <button className='btn btn-dark' onClick={() => quitarProducto(producto)}>Eliminar</button>
                 </div>
             </div>
-            )
+        )
         setCarritoLocal(prodMostrar)
     }, [carrito]);
 
-    const app = (carrito.length != 0) ? <div className='row'> {carritoLocal} </div> :
-        <>
-            <h1>No existen elementos en el carrito</h1>
-                <button className='btn btn-dark'><Link className='nav-link' to={`/`}>Ir al Home</Link></button>
-        </>
+
+    const app = (carrito.length != 0) ? 
+    <div className='row'> {carritoLocal} 
+    <button><Link className='nav-link' to={`/DatosComprador`}>Finalizar Compra</Link></button>
+    <button><Link className="nav-link" onClick={() => cleanCart()}>Limpiar Carrito</Link></button></div> : <> <h1 className='containerProductos'>Carrito Vacio</h1></>
+    
 
     return app
 
